d48ea0d148d3a24f3ac32f08a86ea06907feb495
Anthony Nguyen <antnguye@umich.edu>
2023-02-22 11:13:18 -0500

    updated unit test for read_qm2

--- qm2_human_rarity/tests/test_read_in_qm2.py
+++ qm2_human_rarity/tests/test_read_in_qm2.py
@@ -17,8 +17,7 @@ def test_read_in_qm2_fake_sample():
     output_table, output_dict = compare_against_1000.read_in_qm2(test_file)
     test_answer = np.zeros((1, 2300498), dtype=np.single)
     test_answer.fill(2.4)
-    assert (
-            test_answer == output_table).all(), "Sample should be a np.table of length 2300498 and have 2.4 as all entries."
+    assert (test_answer == output_table).all(), "Sample should be a np.table of length 2300498 and have 2.4 as all entries."
 
 
 def test_read_in_qm2_real_sample():
